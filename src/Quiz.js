eeaf124f05016eb9d751eda3f57d96191b3d2818
Rod Oliveira <jrodolfo@gmail.com>
2019-05-06 15:06:30 -0300

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,8 +13,7 @@ class Quiz extends Component {
         const isQuizEnd = true; // ((this.state.quiz_position - 1) == quizData.quiz_questions.length);
         return (
             <div>
-                {isQuizEnd ? <QuizEnd></QuizEnd> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+                {isQuizEnd ? <QuizEnd></QuizEnd> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
             </div>
         )
     }
